2489ee550709f044399e95005a21b3506cd0bdc7
Mohammed Taitai <mohammedtaitai@outlook.com>
2023-08-10 23:12:51 -0400

    home page finished

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,17 +4,17 @@ import portrait from "../ProfilePic.jpeg";
 function Home() {
   return (
     <div className="Home-container">
+      <div className="Socials">
+        <Socials />
+      </div>
       <div className="introduction">
         <p>Hello, I'm</p>
         <h1>Mohammed Taitai</h1>
         <p>Full stack Developer</p>
-        <button class="Button">About Me</button>
+        <button class="about-button">About Me</button>
       </div>
       <div className="myPic-container">
         <img src={portrait} class="myPic" alt="Mohammed Taitai"></img>
-      </div>
-      <div className="Socials">
-        <Socials />
       </div>
     </div>
   );
