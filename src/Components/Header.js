81179be98609d0b8a95180f8decb4a219bf47e81
Mohammed Taitai <mohammedtaitai@outlook.com>
2023-08-11 09:06:00 -0400

    logo lead to the home page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar'
 import logo from '../logo.png'
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
-    <header className='header'>
-        <img src={logo} alt='Logo'></img>
-        <Navbar />
+    <header className="header">
+      <Link to="/">
+        <img src={logo} alt="Logo"></img>
+      </Link>
+      <Navbar />
     </header>
-  )
+  );
 }
 
 export default Header
