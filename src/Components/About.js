8e6f115e42283a4a662b67a3cff4ac8c57c3b071
Mohammed Taitai <mohammedtaitai@outlook.com>
2024-04-07 13:44:11 -0400

    updated the about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,15 +2,48 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <h4>Skills</h4>
-      <ul>
-        <li>Ruby on Rails</li>
-        <li>React.js</li>
-        <li>C++</li>
-        <li></li>
-      </ul>
+    <div className='about-page'>
+      <h1>About Moh</h1>
+      <p>
+        Based in the vibrant community of Sarasota, Florida, I am a dedicated
+        software developer whose journey in the tech industry is marked by a
+        blend of academic excellence and self-directed learning. My academic
+        foundation was laid at the State College of Florida, where I graduated
+        with an A.S. in Computer Programming and Analysis. My passion for deeper
+        practical knowledge led me to Flatiron School's Software Development
+        Boot Camp, where I refined my skills and discovered an enduring love for
+        coding challenges. Beyond the classroom, my relentless pursuit of
+        knowledge and self-taught proficiency in various programming languages
+        showcase my adaptability and ability to thrive in the ever-evolving tech
+        landscapes.<br></br>
+        My particular passion lies in web development, where I enjoy turning
+        complex problems into beautiful, functional web applications. This
+        enthusiasm is underpinned by my ability to adapt quickly, think
+        critically to solve problems, and manage time efficiently, ensuring
+        projects move forward smoothly and meet their deadlines.<br></br>
+        As for my philosophy towards software development, it revolves around
+        the belief that technology should make life simpler and more efficient,
+        not more complicated. I strive to create software solutions that are
+        both innovative and intuitive, ensuring that technology serves as a
+        bridge to solving real-world problems rather than a barrier. This
+        philosophy fuels my long-term goal of building my own SaaS company,
+        where I aim to develop solutions that empower individuals and businesses
+        to achieve their fullest potential.<br></br> In everything I do, I bring
+        an eagerness to learn and a commitment to excellence. I believe that the
+        journey of education is never complete, and I continuously seek out new
+        technologies, methodologies, and best practices to stay at the forefront
+        of the software development field. My journey is one of constant growth,
+        driven by a vision to not only evolve as a developer but also to
+        contribute meaningfully to the tech community and beyond.{" "}
+      </p>
+      <div className="skillslist">
+        <h4>Skills</h4>
+        <ul>
+          <li>Ruby on Rails</li>
+          <li>React.js</li>
+          <li>C++</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,8 +4,16 @@ function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
+      <div className="projectListContainer">
+        <div className='projectList'>
+          <ul>
+            <li>Project 1</li>
+            <li>Project 2</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Portfolio
