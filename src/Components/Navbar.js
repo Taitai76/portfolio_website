261b795d276e79d9d43c64595010740020899bb8
Mohammed Taitai <mohammedtaitai@outlook.com>
2024-05-04 23:29:33 -0400

    changed resume file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/portfolio">Portfolio</Link>
-      <a href="/Personal/portfolio_website/public/RME!.docx" download>
+      <a href="/Personal/portfolio_website/public/Resume1.pdf" download>
         Resume
       </a>
     </ul>
