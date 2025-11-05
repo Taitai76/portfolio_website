ecd84da981f7c2f1d50c16c1f164a8e2e7165bff
Mohammed Taitai <mohammedtaitai@Mos-MB.local>
2025-11-05 12:52:42 -0700

    added projects

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,13 @@
-
 import React from 'react';
 import pic from "../aboutPic.jpeg";
-import newPic from "../newPic.png";  
+import newPic from "../newPic.png"; 
 
 function About() {
+  const aboutParagraphs = [
+    "Based in the vibrant community of Sarasota, Florida, I am a dedicated software developer whose journey in the tech industry is marked by a blend of academic excellence and self-directed learning. My academic foundation was laid at the State College of Florida, where I graduated with an A.S. in Computer Programming and Analysis. My passion for deeper practical knowledge led me to Flatiron School's Software Development Boot Camp, where I refined my skills and discovered an enduring love for coding challenges. Beyond the classroom, my relentless pursuit of knowledge and self-taught proficiency in various programming languages showcase my adaptability and ability to thrive in the ever-evolving tech landscapes.",
+    "My particular passion lies in web development, where I enjoy turning complex problems into beautiful, functional web applications. This enthusiasm is underpinned by my ability to adapt quickly, think critically to solve problems, and manage time efficiently, ensuring projects move forward smoothly and meet their deadlines.",
+    "As for my philosophy towards software development, it revolves around the belief that technology should make life simpler and more efficient, not more complicated. I strive to create software solutions that are both innovative and intuitive, ensuring that technology serves as a bridge to solving real-world problems rather than a barrier.",
+  ];
   return (
     <div className="about-page">
       <div className="text-and-image">
--- src/data/projects.js
+++ src/data/projects.js
@@ -9,5 +9,25 @@ export default [
     demo: "https://Amvill.co",
     repo: "https://github.com/Taitai76/amvill-emr",
   },
+  {
+    id: 2,
+    title: "Recipe Finder",
+    description:
+      "My first little project. A simple static website that will give you a recipe based on ingredients you input.",
+    tech: ["HTML", "JS", "CSS"],
+    image: "./book.png",
+    demo: "https://simplemealmaker.netlify.app/",
+    repo: "https://github.com/Taitai76/Meal-App",
+  },
+  {
+    id: 3,
+    title: "Spending calculator",
+    description:
+      "My first project using react. A simple react app used to simply calculate your expenses and let you know what you have left.",
+    tech: ["React", "JS", "CSS"],
+    image: "./calc.png",
+    demo: "https://spendintracker.netlify.app/",
+    repo: "https://github.com/Taitai76/SpendinTracker",
+  },
   // add more entries here
 ];
